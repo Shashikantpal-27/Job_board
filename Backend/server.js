0670270cdb11afdfc20c33bb951164d06d1cad4c
Shashikant Pal <shashikantpalspal7518@gmail.com>
2026-02-09 18:28:18 +0530

    JWT auth middleware and protected routes implemented

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const db = require("./config/db");
+const protectedRoutes = require("./routes/protectedRoutes");
 
 const app = express();
 const authRoutes = require("./routes/authRoutes");
@@ -16,6 +17,8 @@ app.get("/", (req, res) => {
   res.send("Job Board Backend Running");
 });
 app.use("/api/auth", authRoutes);
+
+app.use("/api", protectedRoutes);
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
