d6d7106e43b5f7616aad885942c114d9492d66f1
Shashikant Pal <shashikantpalspal7518@gmail.com>
2026-02-09 18:17:26 +0530

    Login API with JWT authentication working

---
+++ Backend/Controllers/authController.js
@@ -0,0 +1,67 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const db = require("../config/db");
+
+exports.register = async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  if (!name || !email || !password || !role) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const sql =
+      "INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?)";
+
+    db.query(sql, [name, email, hashedPassword, role], (err) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ message: "User already exists or DB error" });
+      }
+
+      res.status(201).json({ message: "User registered successfully" });
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields required" });
+  }
+
+  const sql = "SELECT * FROM users WHERE email = ?";
+
+  db.query(sql, [email], async (err, results) => {
+    if (err || results.length === 0) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const user = results[0];
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      role: user.role
+    });
+  });
+};
+
--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,21 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
+const cors = require("cors");
+const db = require("./config/db");
 
+const app = express();
+const authRoutes = require("./routes/authRoutes");
+
+
+app.use(cors());
 app.use(express.json());
+
+
 
 app.get("/", (req, res) => {
   res.send("Job Board Backend Running");
 });
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.use("/api/auth", authRoutes);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
