e4e457385158a70c41f8a306fcdb10851341fab5
Shashikant Pal <shashikantpalspal7518@gmail.com>
2026-06-15 00:26:39 +0530

    fix eslint error

--- frontend/src/pages/EditJob.js
+++ frontend/src/pages/EditJob.js
@@ -22,7 +22,7 @@ function EditJob() {
     axios
       .get("http://localhost:5000/api/jobs")
       .then((res) => {
-        const job = res.data.find((j) => j.id == id);
+        const job = res.data.find((j) => j.id === id);
 
         if (job) {
           setForm({
