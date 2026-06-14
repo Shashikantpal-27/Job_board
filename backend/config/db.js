07c5179469e99d92717845195eaaafb18e540109
Shashikant Pal <shashikantpalspal7518@gmail.com>
2026-06-14 23:55:22 +0530

    deployment update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,15 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error("❌ DB Error:", err.message);
+    console.error("❌ DB Error:", err);
     process.exit(1);
   }
 };
