ff8ab5fb7248949f3a8cca9acc47feb01ebd9bcd
Shashikant Pal <shashikantpalspal7518@gmail.com>
2026-06-22 13:29:14 +0530

    Fix API base URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://job-board-1555.onrender.com",
+  baseURL: "https://job-board-1555.onrender.com/api",
 });
 
 // 🔥 ADD THIS (VERY IMPORTANT)
